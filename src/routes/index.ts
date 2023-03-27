7e7b16844b6399d8a13fb4ad100e4cd1830f112e
Rick504 <rick.wes1@gmail.com>
2023-03-27 16:17:13 -0300

    criando componente dashboard

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,27 +1,36 @@
 /*================================= Imports do VueRouter =================================*/
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from 'vue-router';
 
 /*================================= Imports do Componente Principal =================================*/
-import HomeView from '@/views/Home.vue'
-import LoginView from '@/views/Login.vue'
+import HomeView from '@/views/Home.vue';
+import LoginView from '@/views/Login.vue';
+import Dashboard from '@/views/Dashboard.vue';
 
 const rotas: RouteRecordRaw[] = [
-	{
-		path: '/',
-		name: 'Home',
-		component: HomeView
-	}, {
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView,
+  },
+  {
     path: '/login',
     name: 'Login',
-    component: LoginView
-  }
-
-]
-
+    component: LoginView,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: rotas
-})
+  routes: rotas,
+});
 
-export default router
+export default router;
