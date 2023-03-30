8f231234f4c6a8413bb8384b0dbc2d37570a7650
Rick504 <rick.wes1@gmail.com>
2023-03-30 13:54:15 -0300

    add estilos em botoes de listagens

--- src/views/CadastroMarca.vue
+++ src/views/CadastroMarca.vue
@@ -2,16 +2,16 @@
   <div class="container d-flex align-items-center justify-content-center">
     <nav>
       <ul class="nav">
-        <li class="nav-item">
-          <RouterLink to="/marcas/listar" class="nav-link">Listar Marcas</RouterLink>
+        <li class="nav-item px-5">
+          <RouterLink to="/marcas/listar" class="btn btn-secondary">Listar Marcas</RouterLink>
         </li>
         <li class="nav-item">
-            <RouterLink to="/marcas/cadastrar" class="nav-link">Adicionar marca</RouterLink>
-          </li>
+          <RouterLink to="/marcas/cadastrar" class="btn btn-secondary">Adicionar marca</RouterLink>
+        </li>
       </ul>
     </nav>
   </div>
-<RouterView />
+  <RouterView />
 </template>
 
 <script setup lang="ts">
